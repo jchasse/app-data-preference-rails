91127673d3b817799dc9ca6dab3fd10b28f3d74c
jchasse <jonathan.r.chasse@gmail.com>
2020-09-28 13:45:22 -0500

    added in user_params for strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def new
-
+        @user = User.new
     end
 
     def create
@@ -24,4 +24,16 @@ class UsersController < ApplicationController
 
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :first_name, 
+            :last_name, 
+            :email, 
+            :mobile_number, 
+            :password, 
+            :password_confirmation
+        )
+    end
 end
