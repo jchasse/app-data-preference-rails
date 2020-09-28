cbaec0e093890c48b26b11262cc9fd896947d7ee
jchasse <jonathan.r.chasse@gmail.com>
2020-09-28 10:46:31 -0500

    added in db columns for attributes

--- db/migrate/20200928153815_create_accounts.rb
+++ db/migrate/20200928153815_create_accounts.rb
@@ -1,6 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
+      t.string :org_name
+      t.string :website
+      t.string :toll_free_number
 
       t.timestamps
     end
--- db/migrate/20200928153851_create_digitalprints.rb
+++ db/migrate/20200928153851_create_digitalprints.rb
@@ -1,6 +1,9 @@
 class CreateDigitalprints < ActiveRecord::Migration[6.0]
   def change
     create_table :digitalprints do |t|
+      t.string :kind
+      t.integer :user_id
+      t.integer :account_id 
 
       t.timestamps
     end
