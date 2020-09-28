0ad9c5a9214650e774865bdfd3944f0d7b44d0d4
jchasse <jonathan.r.chasse@gmail.com>
2020-09-28 14:45:32 -0500

    added in edit/update functionality for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,16 @@ class UsersController < ApplicationController
     end
 
     def edit
-
+        @user = User.find_by(id: params[:id]) #can this be tied to current_user?
     end
 
     def update
-
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+        end
     end
 
     def destroy
