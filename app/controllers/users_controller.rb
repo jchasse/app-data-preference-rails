30d416dd66d9edb1a56c9cf61ae66769a7a7276a
jchasse <jonathan.r.chasse@gmail.com>
2020-10-01 08:23:45 -0500

    update partial for userr edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        byebug
         if @user = User.create(user_params)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
