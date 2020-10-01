a46cdb30eb17a0b4e5b403f098a48ae25a465632
jchasse <jonathan.r.chasse@gmail.com>
2020-10-01 08:54:54 -0500

    added full_name method helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
     has_many :accounts, through: :digitalprints
     has_secure_password
 
+
+    def full_name
+        first_name + " " + last_name
+    end
+
+
 end
