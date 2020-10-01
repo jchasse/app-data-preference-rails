a9ddda946dc4027c7aebf4971261143bd27aa2f7
jchasse <jonathan.r.chasse@gmail.com>
2020-10-01 11:13:56 -0500

    removed form duplication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
     end
 
     def create
-        byebug
         if @user = User.create(user_params)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,13 @@
 class Account < ApplicationRecord
     has_many :digitalprints	
     has_many :users, through: :digitalprints
+
+
+    # def account_org_name=(org_name)
+    #     self.account = Category.find_or_create_by(org_name: org_name)
+    # end
+    
+    # def account_org_name
+    #      self.account ? self.account.org_name : nil
+    # end
 end
